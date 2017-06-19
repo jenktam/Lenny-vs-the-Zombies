b5dd4be558b32425c4135ac086f3b7f8af8ad923
jenktam <jennifer.k.tam@gmail.com>
2017-06-18 23:12:55 -0700

    chore(refined goal): Shows text that he reached goal point but still need to load next level

---
+++ Assets/Scripts/Goal/OnGoalEnter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnGoalEnter : MonoBehaviour {
+
+    public Text wonLevelText;
+    public Text goalText;
+    public Canvas nextLevelCanvas;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            goalText.text = "";
+            wonLevelText.text = "Lenny was able to run away from the zombies this time. Lenny runs to the castle to warn the rest of his town before it's too late!";
+            //Destroy(other.gameObject);
+            StartCoroutine(LoadLevel("test", 10.0f));
+        }
+    }
+
+    IEnumerator LoadLevel(string level, float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Application.LoadLevel("Level02");
+    }
+
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel("Level02");
+    }
+}
