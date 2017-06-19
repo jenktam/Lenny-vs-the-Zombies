6eb16251c3829ffadd9aaefabebb5ad3527159c5
jenktam <jennifer.k.tam@gmail.com>
2017-06-18 21:08:30 -0700

    feat(sound toggle button): button in menu settings working

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -8,16 +8,25 @@ public class Menu : MonoBehaviour
     public Canvas MenuCanvas;
     public Canvas OptionsCanvas;
 
+    private bool _isMuted = false;
+
     void Awake()
     {
         OptionsCanvas.enabled = false;
 
     }
-
+    //Options Section
     public void OptionsOn()
     {
         OptionsCanvas.enabled = true;
         MenuCanvas.enabled = false;
+    }
+
+    // Use this for initialization
+    public void ToggleSound()
+    {
+        _isMuted = !_isMuted; // inverts the value. false -> true. true -> false
+        AudioListener.volume = _isMuted ? 1f : 0f; // ? operator allows you to assing values depending on a boolean expression (expression?true:false;)
     }
 
     public void ReturnOn()
@@ -26,6 +35,7 @@ public class Menu : MonoBehaviour
         MenuCanvas.enabled = true;
     }
 
+    //Menu Section
     public void LoadOn()
     {
         Application.LoadLevel(1);
